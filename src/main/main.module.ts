f0fe60ffbbd244fef85331c243ab98b31986ee20
ssjoy43@gmail.com <ssjoy43@gmail.com>
2026-03-24 13:52:00 +0600

    ✨ feat: Add post and profile modules

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UploadModule } from './upload/upload.module';
 import { MessangerModule } from './messanger/messanger.module';
+import { PostModule } from './post/post.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule, UploadModule, MessangerModule],
+  imports: [AuthModule, UploadModule, MessangerModule, PostModule, ProfileModule],
 })
 export class MainModule {}
